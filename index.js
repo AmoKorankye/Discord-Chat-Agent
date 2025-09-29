dbdafbbcf33410ceb5cfa82741bd877f1b2754c4
Kwaku Amo-Korankye <korankye2004@gmail.com>
2025-09-29 11:39:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+// SLASH COMMANDS - explore later implementation
+// client.on(Events.InteractionCreate, async interaction => {
+//   if (!interaction.isChatInputCommand()) return;
+//
+//   if (interaction.commandName === 'ping') {
+//     await interaction.reply('Pong!');
+//   }
+// });
+
+client.login(process.env.DISCORD_TOKEN);
