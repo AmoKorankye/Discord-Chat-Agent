01441e4110b9ab312d3cdc9aab7d617a107affc8
Kwaku Amo-Korankye <korankye2004@gmail.com>
2025-09-30 23:47:06 +0000

    slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong! 🏓',
+  },
+];
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+console.log('🚀 Started refreshing application (/) commands...');
+
+try {
+  const data = await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands },
+  );
+
+  console.log(`✅ Successfully reloaded ${data.length} application (/) commands.`);
+} catch (error) {
+  console.error('❌ Error deploying commands:', error);
+}
--- index.js
+++ index.js
@@ -13,13 +13,13 @@ client.on(Events.ClientReady, readyClient => {
 });
 
 // SLASH COMMANDS - explore later implementation
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
+// client.on(Events.InteractionCreate, async interaction => {
+//   if (!interaction.isChatInputCommand()) return;
+//
+//   if (interaction.commandName === 'ping') {
+//     await interaction.reply('Pong!');
+//   }
+// });
 
 client.on(Events.MessageCreate, async message => {
   console.log(message.content);
